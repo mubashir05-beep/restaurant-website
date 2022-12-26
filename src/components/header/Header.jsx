fbc086be5708b83a95a3ca07e13f4a17d49ddb59
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2022-12-25 21:09:32 -0800

    added and made the header section responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './header.scss'
-
+import headerWelcome from '../../assets/welcome.png'
+import spoonPNG from '../../assets/spoon.png'
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <div className="Restaurant--Header__Container">
+      <div className="Restaurants-Header__left--Section">
+        <div className="Restaurants-Header__left--Section__upper--sub__title">
+          <p className="Restaurants-Header__left--Section--sub__tag">
+            Chase The New Flavour
+          </p>
+          <img src={spoonPNG} alt="" srcset="" />
+        </div>
+        <div className="Restaurants-Header__left--Section__midText">
+          <h1 className="midText__heading">
+            The Key To <br /> Fine Dining
+          </h1>
+          <p className="midText__para">
+            Sit tellus lobortis sed senectus vivamus molestie. Condimentum
+            volutpat morbi facilisis quam scelerisque sapien. Et, penatibus
+            aliquam amet tellus.
+          </p>
+        <button className='midText__btn'>Explore Menu</button>
+        </div>
+      </div>
+      <div className="Restaurants-Header__right--Section">
+        <img src={headerWelcome} alt="Welcome" srcset="" />
+      </div>
+    </div>
+  );
 }
 
 export default Header
